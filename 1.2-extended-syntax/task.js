cfce22505b7eef872900f3d0157cc3c1f58aa3d8
Yuliya Dyakonova <YDyakonova92@gmail.com>
2020-09-25 19:52:42 +0300

    Выполнена

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,49 @@
+"use stric";
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let discriminant = (b ** 2) - (4 * a * c);
+    let x = [];
+    if (discriminant === 0) {
+        x = [-b / (2 * a)];
+    } else if (discriminant > 0) {
+        x.push((-b + (Math.sqrt(discriminant))) / (2 * a));
+        x.push((-b - (Math.sqrt(discriminant))) / (2 * a));  
+    }
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    if (marks.length === 0){
+        return 0;
+    } else if (marks.lenght > 5){
+        console.log('Количество оценок больше 5');
+        marks.splice(5);
+    }
+    let sum = 0;
+    for (let i= 0; i < marks.length; i++){
+        sum += marks[i];
+      }
+      let averageMark = sum / marks.length;
+      return averageMark; 
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let result;
+    let differenceAge = new Date().getFullYear() - dateOfBirthday.getFullYear();
+    let canDrink = `Не желаете ли олд-фэшн, ${name}?`;
+    let notCanDrink = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    result = notCanDrink;
+
+    if (differenceAge > 18) {
+        result = canDrink;
+    } else if (differenceAge == 18) {
+        
+        if (dateOfBirthday.getMonth() < new Date().getMonth()) {
+            result = canDrink;
+        }
+        if ((dateOfBirthday.getMonth() == new Date().getMonth()) && (new Date().getDate() >= dateOfBirthday.getDate())) {            
+            result = canDrink;
+        }
+    }   
+    return result;
 }

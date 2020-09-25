0a48d845f0e85ce909504c56c023885ac2b007ef
Yuliya Dyakonova <YDyakonova92@gmail.com>
2020-09-25 19:57:18 +0300

    Выполнена

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,49 +1,53 @@
-"use stric";
+'use strict'
 
-function getResult(a,b,c){
-    let discriminant = (b ** 2) - (4 * a * c);
-    let x = [];
-    if (discriminant === 0) {
-        x = [-b / (2 * a)];
-    } else if (discriminant > 0) {
-        x.push((-b + (Math.sqrt(discriminant))) / (2 * a));
-        x.push((-b - (Math.sqrt(discriminant))) / (2 * a));  
-    }
-    return x;
+function getResult(a, b, c){
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  let array = [];
+	
+  if (discriminant === 0) {
+    let rootOne = -b / 2 * a;
+    array.push(rootOne);
+  } else if (discriminant > 0) {
+    let rootOneOfTwo = (-b + Math.sqrt(discriminant)) / 2 * a;
+    array.push(rootOneOfTwo);
+    let rootTwoOfTwo = (-b - Math.sqrt(discriminant)) / 2 * a;
+    array.push(rootTwoOfTwo);
+  }
+	
+  let result = array;
+	
+  return result;
 }
 
 function getAverageMark(marks){
-    if (marks.length === 0){
-        return 0;
-    } else if (marks.lenght > 5){
-        console.log('Количество оценок больше 5');
-        marks.splice(5);
-    }
+  let averageMark = 0;
+  let arrayMark = marks;
+	
+  if (marks.length == 0) {
+    return averageMark;
+  }
+	
+  if (marks.length > 5) {
+    console.log("оценок: " + marks.length);  // да
+    arrayMark = marks.slice(0, 5)
+  }
+ 
+  if (marks.length > 0) {
     let sum = 0;
-    for (let i= 0; i < marks.length; i++){
-        sum += marks[i];
-      }
-      let averageMark = sum / marks.length;
-      return averageMark; 
+    for (let i = 0; i < arrayMark.length; i++) {
+      sum += arrayMark[i];
+    }
+
+    return sum / arrayMark.length;
+  }
 }
 
-function askDrink(name,dateOfBirthday){
-    let result;
-    let differenceAge = new Date().getFullYear() - dateOfBirthday.getFullYear();
-    let canDrink = `Не желаете ли олд-фэшн, ${name}?`;
-    let notCanDrink = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
-    result = notCanDrink;
+function askDrink(name, dateOfBirthday){
+  let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
 
-    if (differenceAge > 18) {
-        result = canDrink;
-    } else if (differenceAge == 18) {
-        
-        if (dateOfBirthday.getMonth() < new Date().getMonth()) {
-            result = canDrink;
-        }
-        if ((dateOfBirthday.getMonth() == new Date().getMonth()) && (new Date().getDate() >= dateOfBirthday.getDate())) {            
-            result = canDrink;
-        }
-    }   
-    return result;
+  if (age >= 18) {
+    return `Не желаете ли олд-фэшн, ${name}?`;
+  } else {
+    return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+  }
 }

454c777bf63f7c619cefdb04f64418d98bc99c7b
Yuliya Dyakonova <YDyakonova92@gmail.com>
2020-09-26 12:48:14 +0300

    Выполнено

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,23 @@
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+    let credit = Number(amount) - Number(contribution);
+    let monthNumber = (date.getFullYear() - new Date().getFullYear()) * 12;
+    let P = (1 / 12) * (Number(percent) / 100);
+    let payment = credit * (P + P / (Math.pow(1 + P, monthNumber) - 1));
+    let totalAmount = payment * monthNumber;
+
+    console.log(totalAmount.toFixed(2));
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    
+    if(name){
+        console.log("Привет, мир! Меня зовут ${name}");
+        return "Привет, мир! Меня зовут ${name}";
+    } else {
+        console.log ("Привет, мир! Меня зовут Аноним");
+        return "Привет, мир! Меня зовут Аноним";
+    }
 }
